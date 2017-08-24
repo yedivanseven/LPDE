d5107dee25cbd2ad32c51e0d1a93569d87d6fe5f
Yedi van Seven <georg@muckisnspirit.com>
2017-08-24 16:02:53 +0200

    Renamed initial coefficients

--- lpde/estimators/datatypes/__init__.py
+++ lpde/estimators/datatypes/__init__.py
@@ -2,5 +2,5 @@ from .action import Action
 from .coefficients import Coefficients
 from .degree import Degree
 from .event import Event
-from .initialcoefficients import InitialCoefficients
+from .initial_coefficients import InitialCoefficients
 from .scalings import Scalings
