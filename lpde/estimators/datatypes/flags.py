7aa916c6cbec9fa46f4d926598e28ab9b14d0fbf
Yedi van Seven <georg@muckisnspirit.com>
2017-08-29 23:04:51 +0200

    Minor refactoring

--- lpde/estimators/datatypes/flags.py
+++ lpde/estimators/datatypes/flags.py
@@ -17,3 +17,14 @@ class Flags:
     @property
     def any_set(self) -> bool:
         return any((self.__stop.is_set(), self.__done.is_set()))
+
+
+if __name__ == '__main__':
+    flag = Flags()
+    print(flag.stop.is_set())
+    flag.stop.set()
+    print(flag.stop.is_set())
+    flag.stop.clear()
+    print(flag.stop.is_set())
+    flag.done.set()
+    print(flag.done.is_set())
